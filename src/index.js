78b81b7945090e4e7a8c23ab235a9b4ee5733789
Mateusz Ossolinski <itsmathev@gmail.com>
2018-05-12 10:20:27 +0200

    End first seciton of course

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBw37dPXpTpB5-DyCKuAw3PHohR7PNAP4s';
+
 const App = () => {
-    return <div> Hi! </div>;
+    return <div>
+                <SearchBar />
+           </div>;
 }
 
 ReactDOM.render(<App />,
