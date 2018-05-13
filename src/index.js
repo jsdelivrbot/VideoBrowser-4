936c8add93724ae015c60bd87407999eebc3dfbe
Mateusz Ossolinski <itsmathev@gmail.com>
2018-05-13 21:22:45 +0200

    Add search response in main component, refactor const to class component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBw37dPXpTpB5-DyCKuAw3PHohR7PNAP4s';
 
-const App = () => {
-    return <div>
-                <SearchBar />
-           </div>;
+
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+            this.setState({ videos });
+            //this.setState({ videos: videos })
+        });
+    }
+
+    render(){
+        return (
+                <div>
+                    <SearchBar />
+            </div>
+        );
+    } 
 }
 
 ReactDOM.render(<App />,
