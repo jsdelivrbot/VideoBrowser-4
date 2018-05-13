30b2f66ca7dc6710ffc35ff3bbdd2fc517f35bac
Mateusz Ossolinski <itsmathev@gmail.com>
2018-05-13 21:58:31 +0200

    Add video list components, template list of videos

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyBw37dPXpTpB5-DyCKuAw3PHohR7PNAP4s';
-
 
 
 class App extends Component {
@@ -13,7 +13,7 @@ class App extends Component {
 
         this.state = { videos: [] };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
             this.setState({ videos });
             //this.setState({ videos: videos })
         });
@@ -23,6 +23,7 @@ class App extends Component {
         return (
                 <div>
                     <SearchBar />
+                    <VideoList videos={this.state.videos}/>
             </div>
         );
     } 
