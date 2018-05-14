623a9a4f44ba8054080072c3ca818152ca8dc5a6
Mateusz Ossolinski <itsmathev@gmail.com>
2018-05-14 15:10:20 +0200

    Add styles of app, make work search bar

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -9,7 +9,7 @@ const VideoDetail = ({video}) => {
     const url = `https://www.youtube.com/embed/${videoId}`;
 
     return (
-        <div className="video-detail col-md-8">
+        <div className="video-detail col-md-6">
             <div className="embed-resposnive">
                 <iframe className="embed-responsive-item" src={url}></iframe>
             </div>
